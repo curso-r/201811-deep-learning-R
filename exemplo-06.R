9facbdade4437aa83b5404f8f7817e60c18a699b
Daniel Falbel <dfalbel@gmail.com>
2018-11-10 00:05:16 -0200

    mais exemplos

--- exemplo-06.R
+++ exemplo-06.R
@@ -1,21 +1,47 @@
-# Pacotes -----------------------------------------------------------------
-
 library(keras)
-library(tidyverse)
 
-# Dados -------------------------------------------------------------------
+x <- sample(c(letters, LETTERS, 0:9), size = 10000, replace = TRUE)
+x_int <- as.integer(as.factor(x)) - 1L 
+y <- rnorm(10000, mean = x_int, sd = 2)
 
-imdb <- dataset_imdb()
-imdb_words <- dataset_imdb_word_index()
-words <- imdb_words %>% imap_dfr(~dplyr::data_frame(id = .x, palavra = .y))
+tapply(y, x, mean)
 
-imdb$train$x[[1]] %>% 
-  data_frame(id = . - 3) %>% 
-  left_join(words, by = "id") %>% 
-  with(palavra) %>% 
-  paste(collapse = " ")
+x2 <- to_categorical(x_int)
 
-x_train <- imdb$train$x %>%
-  pad_sequences(maxlen = 400)
-x_test <- imdb$test$x %>%
-  pad_sequences(maxlen = 400)
+# Com one-hot encoding ----------------------------------------------------
+
+input <- layer_input(shape = c(62))
+output <- input %>% 
+  layer_dense(32, use_bias = FALSE) %>% 
+  layer_dense(1)
+
+
+model <- keras_model(input, output)
+model %>% 
+  compile(
+    loss = "mse",
+    optimizer = "sgd"
+  )
+
+model %>% 
+  fit(x2, y, validation_split = 0.2)
+
+
+# Com embedding -----------------------------------------------------------
+
+input <- layer_input(shape = c(1))
+output <- input %>% 
+  layer_embedding(input_dim = 62, output_dim = 32) %>% 
+  layer_flatten() %>% 
+  layer_dense(1)
+
+model_emb <- keras_model(input, output)
+
+model_emb %>% 
+  compile(
+    loss = "mse",
+    optimizer = "sgd"
+  )
+
+model_emb %>% 
+  fit(matrix(x_int, ncol = 1), y, validation_split = 0.2)
